d38f49301cfbdb6a7fe28e64d01f2af50c380b92
YuanShenKing.PC <20230404@gmail.com>
2024-04-23 16:45:07 +0800

    first commit

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<cmath>
+#include<cctype>
+#include<iomanip>
+#include<sstream>
+using namespace std;
+
+
+int main(){
+    
+}
